9ac8628b1d62c98ec7af5351a55b3f3bfa05a14c
vqmpjay <bakir.butkovic@osgrbavica2.edu.ba>
2025-06-22 19:44:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const port = process.env.PORT || 3000;
 
 const tokens = {};
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use(express.static('.'));
 
 app.get('/', (req, res) => {
